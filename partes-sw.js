db6201e44f48291e3a7f3330933dc7a968725744
XIMO1800 <joacuque@gmail.com>
2026-06-23 09:24:58 +0200

    partes195

--- partes-sw.js
+++ partes-sw.js
@@ -1,6 +1,6 @@
 // Service Worker para Partes de Fabricación
-// Versión: 2.6.194 - cambiar este número en cada nueva versión del HTML
-const CACHE_VERSION = 'partes-v2-6-194';
+// Versión: 2.6.195 - cambiar este número en cada nueva versión del HTML
+const CACHE_VERSION = 'partes-v2-6-195';
 const CACHE_FILES = [
   'partes-fabricacion.html',
   'partes-manifest.json',
